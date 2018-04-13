c3be8d51de66de08cff73e213df87bb729d2da73
kanottonp <kanottonp@gmail.com>
2018-04-13 23:08:16 +0700

    initial push

---
+++ nuxt.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: 'db-proj',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Team Chim DB Proj' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress bar color
+     */
+    loading: { color: '#3B8070' },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** Run ESLint on save
+         */
+    }
+}
