a9ebd566a1f920af7dabd8ee7a32c162c0ef79bc
kanottonp <kanottonp@gmail.com>
2018-04-28 16:22:39 +0700

    Finish Student Part from Given API

--- pages/student/searchcourse.vue
+++ pages/student/searchcourse.vue
@@ -32,7 +32,34 @@
             </ul>
           </b-col>
           <b-col style="background-color: lightblue">
-            <div style="margin-top: 10px; margin-bottom 3px;"><h1><b>ค้นหารายวิชาเรียน</b></h1></div>
+
+            <div style="margin-top: 10px; margin-bottom: 20px;"><h1><b>ค้นหารายวิชาเรียน</b></h1></div>
+            <b-row>
+          <b-col cols="4">
+            <input placeholder="SubjectID" v-model="subjectID" style="margin-bottom: 10px;">
+            <select>
+              <option value=2016>2016</option>
+              <option value=2017>2017</option>
+              <option value=2018>2018</option>
+            </select>
+            <select>
+              <option value=1>ต้น</option>
+              <option value=2>ปลาย</option>
+              <option value=3>ฤดูร้อน</option>
+            </select>
+            <button @click="seachSubject">ค้นหา</button>
+            <li style="margin-bottom: 2px; cursor: pointer;" v-for="subjects in allSubjects" :key="subjects.SubjectID"><a v-on:click="search(subjects.SubjectID)">{{subjects.SubjectID}} {{subjects.NameEN}}</a></li>
+          </b-col>
+          <b-col>
+            <ul style="margin-bottom: 2px;">{{subject.SubjectID}}</ul>
+            <ul style="margin-bottom: 2px;">{{subject.NameEN}}</ul>
+            <ul style="margin-bottom: 2px;">{{subject.NameTH}}</ul>
+            <ul style="margin-bottom: 2px;">{{subject.MidtermStartDatetime === undefined ? "" : `วันสอบกลางภาค: ${subject.MidtermStartDatetime}` }}</ul>
+            <ul style="margin-bottom: 2px;">{{subject.MidtermEndDatetime}}</ul>
+            <ul style="margin-bottom: 2px;">{{subject.FinalStartDatetime  === undefined ? "" : `วันสอบปลายภาค: ${subject.FinalStartDatetime}`}}</ul>
+            <ul style="margin-bottom: 2px;">{{subject.FinalEndDatetime}}</ul>
+          </b-col>
+          </b-row>
             </b-col>
         </b-row>
       </b-container>
@@ -61,7 +88,10 @@ export default {
   data: function(){
     return {
     allSubjects: [],
-    subject: []
+    subject: {},
+    subjectID: "",
+    year: 2017,
+    semester: 2
     }
   },
 
@@ -95,16 +125,59 @@ export default {
   components: {
   },
 
-  method: {
-    goStudent () {
-      this.$router.push({ path: '/student/main'})
+  methods: {
+    async search (subjectID) {
+
+      this.subject = {}
+
+      var studentid = getCookie('username')
+
+
+      axios.get(API + `/v2/subjects/${subjectID}/${this.year}/${this.semester}`, {
+        headers: {
+          'Authorization' : studentid
+        }
+      })
+        .then((data) => {
+          this.subject = data.data
+          console.log(this.subject)
+      })
+
     },
-    goStudent () {
-      this.$router.push({ path: '/teacher/main'})
-    },
-    goStaff () {
-      this.$router.push({ path: '/staff/main'})
-    },
+
+    async seachSubject () {
+      this.allSubjects = []
+
+      var studentid = getCookie('username')
+
+      if (this.subjectID === "") {
+        axios.get(API + `/v2/subjects/`, {
+        headers: {
+          'Authorization' : studentid
+        }
+        })
+        .then((data) => {
+          data.data.forEach(element => {
+            this.allSubjects.push(element)
+          });
+          console.log(this.allSubjects)
+        })
+      }
+
+      else {
+        axios.get(API + `/v2/subjects/${this.subjectID}/${this.year}/${this.semester}`, {
+        headers: {
+          'Authorization' : studentid
+        }
+      })
+        .then((data) => {
+
+          this.allSubjects.push(data.data)
+
+          console.log(this.allSubjects)
+      })
+      }
+    }
 
 
   }
@@ -121,8 +194,8 @@ export default {
 
 .input-container {
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 20px;
-  padding: 2px 2px 2px 2px;
+  font-size: 5px;
+  padding: 1px 1px 1px 1px;
   border-radius: 3px;
   margin-top: 5px;
   border: 2px solid #ececec
