e3259d84bb27f03a64aa246738e7fc9c6f9f2d85
vbssoloww <vbssoloww@gmail.com>
2018-05-24 10:50:54 +0700

    adding internship and report

--- pages/student/manageinternship.vue
+++ pages/student/manageinternship.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="container">
-      <div>
-       <b-navbar toggleable="md" type="dark" variant="info">
+    <div>
+      <b-navbar toggleable="md" type="dark" variant="info">
         <b-navbar-brand href="/student/main">Student</b-navbar-brand>
-       <b-navbar-nav class="ml-auto">
-         <b-nav-item href="/" right>Signout</b-nav-item>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/" right>Signout</b-nav-item>
         </b-navbar-nav>
       </b-navbar>
       <b-container>
@@ -30,7 +30,69 @@
               <li><a href="/student/searchinternshipreview">ค้นหา Review การฝึกงาน</a></li>
             </ul>
           </b-col>
-          <b-col style="background-color: lightblue">จัดการการฝึกงาน</b-col>
+          <b-col style="background-color: lightblue">
+            <div style="margin-top:10px; margin-bottom: 3px">
+              <h2><b>การฝึกงาน</b></h2>
+            </div>
+            <div style="display: flex; justify-content: center;">
+              <table border="1" style="margin-top: 20px; align-self: center; width:90%">
+                <tr>
+                  <th>ปี</th>
+                  <th>สถานะ</th>
+                  <th>บริษัท</th>
+                  <th>Position</th>
+                  <th>ตำแหน่ง</th>
+                  <th>วันเริ่มต้นการฝึกงาน</th>
+                  <th>วันที่สิ้นสุดการฝึกงาน</th>
+                  <th>Review</th>
+                </tr>
+                <tr v-for="internship in allInternships" :key="internship.Year">
+                  <td>{{ internship.Year}}</td>
+                  <td>{{ internship.Status}}</td>
+                  <td>{{ internship.NameEN}}</td>
+                  <td>{{ internship.PositionNameEn === null ? '-' : internship.PositionNameEn}}</td>
+                  <td>{{ internship.PositionNameTH === null ? '-' : internship.PositionNameTH}}</td>
+                  <td>{{ internship.StartDate}}</td>
+                  <td>{{ internship.EndDate}}</td>
+                  <td>{{ (internship.Comment === null || internship.Comment == "") ? "-" : internship.Comment}}</td>
+                </tr>
+              </table>
+            </div>
+            <div style="margin-top:10px; margin-bottom: 3px">
+              <h2><b>เพิ่มการฝึกงาน</b></h2>
+            </div>
+            <div class="formcontainer" style="display: flex">
+              <form>
+                <p>
+                  <label for="">Year</label>
+                  <input type="text" name="year" v-model="year"><br>
+                </p>
+                <p>
+                  <label for="">Company ID</label>
+                  <input type="text" name="companyid" v-model="companyId"><br>
+                </p>
+                <p>
+                  <label for="">Branch Name</label>
+                  <input type="text" name="branchname" v-model="branchName"><br>
+                </p>
+                <p>
+                  <label for="">Start Date</label>
+                  <input type="text" name="startdate" v-model="startdate"><br>
+                </p>
+                <label for="">End Date</label>
+                <input type="text" name="enddate" v-model="enddate"><br>
+                <p>
+                  <label for="">Position (English)</label>
+                  <input type="text" name="positionnameen" v-model="positionNameEn"><br>
+                </p>
+                <p>
+                  <label for="">Official</label>
+                  <input type="checkbox" name="isofficial" v-model="isOfficial"><br>
+                </p>
+                <button type="button" @click="addIntern">เพิ่ม</button>
+              </form>
+            </div>
+          </b-col>
         </b-row>
       </b-container>
     </div>
@@ -38,59 +100,162 @@
 </template>
 
 <script>
-
-function getCookie(cname) {
+  import Vue from 'vue'
+  import axios from 'axios'
+  import moment from 'moment'
+  const {
+    API
+  } = require('../../api.config');
+  
+  function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1);
+      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
-}
-
-export default {
-  components: {
-  },
-
+  }
+  
+  export default {
+    data: function() {
+      return {
+        allInternships: [],
+        year: "2017",
+        companyId: "2",
+        branchName: "1",
+        startdate: "2017-01-01",
+        enddate: "2017-03-01",
+        positionNameEn: "test frontend",
+        isOfficial: true,
+      }
+    },
+    components: {},
+  
     beforeMount: function() {
-        console.log("hello")
-        console.log(getCookie('username'))
-        if (getCookie('username') === ""){
+      console.log("hello")
+      console.log(getCookie('username'))
+      if (getCookie('username') === "") {
         alert("Session Timeout!")
         this.$router.push('/')
+      }
+  
+    },
+    mounted: function() {
+  
+      this.allInternships = []
+  
+      var studentid = getCookie('username')
+  
+      axios.get(API + `/v2/students/${studentid}/internships`, {
+        headers: {
+          'Authorization': studentid
         }
-  },
-
-  method: {
-    goStudent () {
-      this.$router.push({ path: '/student/main'})
+      }).then((data) => {
+        data.data.forEach(element => {
+          this.allInternships.push(element)
+        });
+        console.log('finish')
+        console.log(this.allInternships);
+      })
     },
-    goStudent () {
-      this.$router.push({ path: '/teacher/main'})
-    },
-    goStaff () {
-      this.$router.push({ path: '/staff/main'})
+  
+    methods: {
+  
+      async addIntern() {
+        var studentid = getCookie('username')
+        console.log(this.startDate);
+        console.log(this.endDate);
+        if (!this.isOfficial) {
+          axios.put(API + `/v2/students/${studentid}/internships`, {
+            headers: {
+              'Authorization': studentid
+            },
+            companyId: this.companyId,
+            branchName: this.branchName,
+            year: this.year,
+            startdate: this.startdate,
+            enddate: this.enddate,
+            positionNameEn: this.positionNameEn,
+            positionNameTh: this.positionNameTh,
+          }).then(function(response) {
+            console.log(response)
+            alert("accepted")
+          }).catch(function(error) {
+            console.log(error)
+            alert("error")
+          })
+        } else {
+          axios.put(API + `/v2/students/${studentid}/official_internship`, {
+            headers: {
+              'Authorization': studentid
+            },
+            companyId: this.companyId,
+            branchName: this.branchName,
+            year: this.year,
+            startdate: this.startdate,
+            enddate: this.enddate,
+            positionNameEn: this.positionNameEn,
+            positionNameTh: this.positionNameTh,
+          }).then(function(response) {
+            console.log(response)
+            alert("accepted")
+          }).catch(function(error) {
+            console.log(error)
+            alert("error")
+          })
+        }
+      },
+  
+      goStudent() {
+        this.$router.push({
+          path: '/student/main'
+        })
+      },
+      goStudent() {
+        this.$router.push({
+          path: '/teacher/main'
+        })
+      },
+      goStaff() {
+        this.$router.push({
+          path: '/staff/main'
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-
-.container1 {
-  min-width : 100vw;
-  min-height: calc(10/16*100vw);
-}
-
-
-.input-container {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 20px;
-  padding: 2px 2px 2px 2px;
-  border-radius: 3px;
-  margin-top: 5px;
-  border: 2px solid #ececec
-}
+  .container1 {
+    min-width: 100vw;
+    min-height: calc(10/16*100vw);
+  }
+  
+  .input-container {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    padding: 2px 2px 2px 2px;
+    border-radius: 3px;
+    margin-top: 5px;
+    border: 2px solid #ececec
+  }
+  
+  form {
+    display: table;
+  }
+  
+  p {
+    display: table-row;
+  }
+  
+  label {
+    display: table-cell;
+    text-align: left;
+  }
+  
+  input {
+    display: table-cell;
+  }
 </style>
--- pages/student/sendinternshipreport.vue
+++ pages/student/sendinternshipreport.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="container">
-      <div>
-       <b-navbar toggleable="md" type="dark" variant="info">
+    <div>
+      <b-navbar toggleable="md" type="dark" variant="info">
         <b-navbar-brand href="/student/main">Student</b-navbar-brand>
-       <b-navbar-nav class="ml-auto">
-         <b-nav-item href="/" right>Signout</b-nav-item>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/" right>Signout</b-nav-item>
         </b-navbar-nav>
       </b-navbar>
       <b-container>
@@ -30,7 +30,52 @@
               <li><a href="/student/searchinternshipreview">ค้นหา Review การฝึกงาน</a></li>
             </ul>
           </b-col>
-          <b-col style="background-color: lightblue">ส่งรายงานการฝึกงาน</b-col>
+          <b-col style="background-color: lightblue">ส่งรายงานการฝึกงาน
+            <div style="margin-top:10px; margin-bottom: 3px">
+              <h2><b>รายงานการฝึกงาน</b></h2>
+            </div>
+            <div style="display: flex; justify-content: center;">
+              <table border="1" style="margin-top: 20px; align-self: center; width:90%">
+                <tr>
+                  <th>ปี</th>
+                  <th>บริษัท</th>
+                  <th>รายงานฉบับที่</th>
+                  <th>ชั่วโมงฝึกงาน</th>
+                </tr>
+                <tr v-for="report in allReports" :key="report.Year">
+                  <td>{{ report.Year }}</td>
+                  <td>{{ report.NameEN }}</td>
+                  <td>{{ report.Number }}</td>
+                  <td>{{ report.Hours }}</td>
+                </tr>
+              </table>
+            </div>
+            <div class="formcontainer" style="display: flex">
+              <form>
+                <p>
+                  <label for="">Year</label>
+                  <input type="text" name="year" v-model="year"><br>
+                </p>
+                <p>
+                  <label for="">Company ID</label>
+                  <input type="text" name="companyid" v-model="companyId"><br>
+                </p>
+                <p>
+                  <label for="">Branch Name</label>
+                  <input type="text" name="branchname" v-model="branchName"><br>
+                </p>
+                <p>
+                  <label for="">Report Number</label>
+                  <input type="text" name="repno" v-model="repNo"><br>
+                </p>
+                <p>
+                  <label for="">Hours</label>
+                  <input type="text" name="hours" v-model="hours"><br>
+                </p>
+                <button type="button" @click="addReport">เพิ่ม</button>
+              </form>
+            </div>
+          </b-col>
         </b-row>
       </b-container>
     </div>
@@ -38,59 +83,149 @@
 </template>
 
 <script>
-
-function getCookie(cname) {
+  import Vue from 'vue'
+  import axios from 'axios'
+  import moment from 'moment'
+  const {
+    API
+  } = require('../../api.config');
+  
+  function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1);
+      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
-}
-
-export default {
-  components: {
-  },
-
+  }
+  
+  export default {
+    data: function() {
+      return {
+        allReports: [],
+        year: "2017",
+        companyId: "2",
+        branchName: "1",
+        repNo: "1",
+        hours: "99",
+      }
+    },
+    components: {},
+  
     beforeMount: function() {
-        console.log("hello")
-        console.log(getCookie('username'))
-        if (getCookie('username') === ""){
+      console.log("hello")
+      console.log(getCookie('username'))
+      if (getCookie('username') === "") {
         alert("Session Timeout!")
         this.$router.push('/')
+      }
+    },
+    mounted: function() {
+      this.allReports = [];
+  
+      var studentid = getCookie('username')
+  
+      axios.get(API + `/v2/students/${studentid}/official_internship/reports`, {
+        headers: {
+          'Authorization': studentid
         }
-  },
-
-  method: {
-    goStudent () {
-      this.$router.push({ path: '/student/main'})
+      }).then((data) => {
+        data.data.forEach(element => {
+          this.allReports.push(element)
+        });
+        console.log('finish')
+        console.log(this.allReports);
+      })
     },
-    goStudent () {
-      this.$router.push({ path: '/teacher/main'})
-    },
-    goStaff () {
-      this.$router.push({ path: '/staff/main'})
+  
+    methods: {
+      getDate() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var yyyy = today.getFullYear();
+  
+        if (dd < 10) {
+          dd = '0' + dd
+        }
+  
+        if (mm < 10) {
+          mm = '0' + mm
+        }
+  
+        today = yyyy + '-' + mm + '-' + dd;
+        console.log(typeof(today));
+        return today;
+      },
+      async addReport() {
+        console.log(this.getDate());
+        var studentid = getCookie('username')
+        axios.put(API + `/v2/students/${studentid}/official_internship/reports`, {
+          headers: {
+            'Authorization': studentid
+          },
+          companyId: this.companyId,
+          branchName: this.branchName,
+          year: this.year,
+          number: this.repNo,
+          hours: this.hours,
+          date: this.getDate(),
+        }).then(function(response) {
+          // console.log(response)
+          // alert("accepted")
+          console.log(response)
+        })
+      },
+      goStudent() {
+        this.$router.push({
+          path: '/student/main'
+        })
+      },
+      goStudent() {
+        this.$router.push({
+          path: '/teacher/main'
+        })
+      },
+      goStaff() {
+        this.$router.push({
+          path: '/staff/main'
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-
-.container1 {
-  min-width : 100vw;
-  min-height: calc(10/16*100vw);
-}
-
-
-.input-container {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 20px;
-  padding: 2px 2px 2px 2px;
-  border-radius: 3px;
-  margin-top: 5px;
-  border: 2px solid #ececec
-}
+  .container1 {
+    min-width: 100vw;
+    min-height: calc(10/16*100vw);
+  }
+  
+  .input-container {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    padding: 2px 2px 2px 2px;
+    border-radius: 3px;
+    margin-top: 5px;
+    border: 2px solid #ececec
+  }
+  
+  form {
+    display: table;
+  }
+  
+  p {
+    display: table-row;
+  }
+  
+  label {
+    display: table-cell;
+    text-align: left;
+  }
+  
+  input {
+    display: table-cell;
+  }
 </style>
